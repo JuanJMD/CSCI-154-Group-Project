a144a70e3b7702a73636a3811e784c4e3fc643e5
Yubo Z. <124237553+Deletenomore@users.noreply.github.com>
2024-04-16 22:45:55 -0700

    Update Monty Hall Moduel
    Use set operation to simulation

--- Project 1/project1.py
+++ Project 1/project1.py
@@ -4,69 +4,43 @@ import numpy as np
 import csv
 import pandas as pd
 
-class door:
-    doorID = 0
-    hasGoat = False
-    car = False
+def montyHall(numDoors, rule, trial):
+    #Create a set of doors by using pre-set number of doors
+    doors = []
+    for x in range(numDoors+1):
+        doors.append(x)
     
-#empty door array
-doorMatrix = []
+    #Assign the car to a door
+    car = secrets.choice(doors)
 
-# Creates n-amount of doors
-def doorCreation(numDoors):
-    for row in range(1, numDoors + 1):
-        dummyDoor = door()
-        dummyDoor.doorID = row
-        dummyDoor.hasGoat = True
-        doorMatrix.append(dummyDoor)
-    return doorMatrix
+    #Guest first choice
+    guest_1st_choice = secrets.choice(doors)
 
-# Places Car randomly
-def placeCar(doorMatrix):
-    carDoor = secrets.choice(doorMatrix)
-    carDoor.hasGoat = False
-    carDoor.car = True
-    return carDoor.doorID
-
-def userFirstChoice(doorMatrix):
-    #using random number to simulate user choice
-    userDoor = secrets.choice(doorMatrix)
-    return userDoor.doorID
-
-#If user always switches the door, we only need to consider if user's first choice contains a car 
-#If yes, switching always win
-#Otherwise, switching always lose
-def alwaysSwitch(userDoor,carDoor):
-    if(userDoor != carDoor):
-        return True
-    else:
-        return False
-
-#If user always stays and the first choice contains a car, the user wins. Otherwise, the user loses
-def alwaysStay(userDoor, carDoor):
-    if(userDoor == carDoor):
-        return True
-    else:
-        return False
-
-def montyHall(numDoors, rule):
-    if not numDoors:
-        return
+    #The doors that host can open, which can only be the goat doors.
+    host_can_open = set(doors) - set([car,guest_1st_choice])
+    openlist=[]
+    #Host opens all other doors and lefts only one door for the guest as the second choice.
+    for y in range (numDoors-1):
+        host_opens = secrets.choice(list(host_can_open -set(openlist)))
+        openlist.append(host_opens)
     
-    #game initializaton
-    doorMatrix=doorCreation(numDoors)
+    guest_2nd_choice = min(set(doors)-set(openlist + [guest_1st_choice]))
+    
+    if trial<=5:
+        print(f"First choice: {guest_1st_choice}, Second choice: {guest_2nd_choice}, Car: {car} ")
+    
+    #Guest Strategies 
+    if rule== 'switch':
+        if car == guest_2nd_choice:
+            result = True
+        else:
+            result = False
 
-    #get the cardoor
-    carDoor = placeCar(doorMatrix)
-
-    # User's first choice
-    userDoor = userFirstChoice(doorMatrix)
-
-    #Update rule choices
-    if rule==1:
-        result = alwaysSwitch(userDoor,carDoor)
-    else:
-        result = alwaysStay(userDoor,carDoor)
+    if rule== 'stay':
+        if guest_1st_choice == car:
+            result = True
+        else:
+            result = False
     
     return result
 
@@ -97,11 +71,13 @@ def barChart(list1,list2,list3,list4):
     x_list2 = [x for x in range(len(list2))]
     x_list3 = [x+width for x in range (len(list3))]
     x_list4 = [x+width*2 for x in range (len(list4))]
+
     fig,ax = plt.subplots()
     bar1 = ax.bar(x_list1,list1,width,label='Win_Switch')
     bar2 = ax.bar(x_list2,list2,width,label='Lose_Switch')
     bar3 = ax.bar(x_list3,list3,width,label='Win_Stay')
-    bar4 = ax.bar(x_list4,list4,width,label='Lose_Stay')
+    bar4 = ax.bar(x_list4,list4,width,label='Lose_Stay')    
+
 
     def autolabel(bars, data):
         for bar, value in zip (bars, data):
@@ -121,7 +97,7 @@ def barChart(list1,list2,list3,list4):
     ax.set_xlabel('Door Secanrio')
     ax.set_ylabel('Probability of 100000 times')
     ax.set_title('Monty Hall Swtiching and Staying Scenario Probability')
-    #plt.savefig('BarChart.png')
+    plt.savefig('BarChart.png')
 
 #create table function
 def plot_table(fileName):
@@ -132,10 +108,10 @@ def plot_table(fileName):
     table.auto_set_font_size(False)
     table.set_fontsize(8)
     table.scale(1,1.5)
-    #plt.savefig("ResultTable.png")
+    plt.savefig("ResultTable.png")
 
 def print_Result(list1,list2,list3,list4):
-    #write_and_plot(list1,list2,list3,list4)
+    write_and_plot(list1,list2,list3,list4)
     barChart(list1,list2,list3,list4)
     plt.show()
 
@@ -144,7 +120,7 @@ def main():
     #Number of doors
     numDoors = [3,6,9,20,100]
     #simulation times
-    times = 100000
+    trials = 100000
     #alway switch probaility
     winProbability1=[]
     loseProbability1=[]
@@ -153,17 +129,19 @@ def main():
     loseProbability2 =[]
 
     for n in numDoors:
+        print(f"Doors = {n}")
         countWin1 = 0
         countLose1 = 0
         #Always Switching Case:
-        for x in range(times+1):
-            if (montyHall(n, 1)):
+        for x in range(trials):
+            if (montyHall(n, 'switch', x)):
+                #print("Switch")
                 countWin1 +=1
             else:
                 countLose1 +=1
         
-        winProbability1.append(round(countWin1/times,2))
-        loseProbability1.append(round(countLose1/times,2))
+        winProbability1.append(round(countWin1/trials,3))
+        loseProbability1.append(round(countLose1/trials,3))
         # print("Door number: ", n)
         # print("Win: ", countWin1)
         # print("Lose: ", countLose1)
@@ -173,13 +151,13 @@ def main():
         #Always Staying Case:
         countWin2 = 0
         countLose2 = 0
-        for x in range(times+1):
-            if (montyHall(n,2)):
+        for x in range(trials):
+            if (montyHall(n,'stay',x)):
                 countWin2 +=1
             else:
                 countLose2 +=1
-        winProbability2.append(round(countWin2/times,2))
-        loseProbability2.append(round(countLose2/times,2))
+        winProbability2.append(round(countWin2/trials,3))
+        loseProbability2.append(round(countLose2/trials,3))
         # print("Door number: ", n)
         # print("Win: ", countWin2)
         # print("Lose: ", countLose2)
