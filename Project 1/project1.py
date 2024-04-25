fe1e4cf408eb58ca2d7775f89136850bc5a6b530
Yubo Z. <124237553+Deletenomore@users.noreply.github.com>
2024-04-25 11:09:55 -0700

    Update code with Monty Fall
    1. Update Code with Monty Fall
    2. Update result
    3. Update probability convergence

--- Project 1/project1.py
+++ Project 1/project1.py
@@ -4,6 +4,8 @@ import numpy as np
 import csv
 import pandas as pd
 import plotly.graph_objects as go
+plt.style.use("fivethirtyeight")
+
 
 def montyHall(numDoors, rule, trial):
     #Create a set of doors by using pre-set number of doors
@@ -45,12 +47,57 @@ def montyHall(numDoors, rule, trial):
     
     return result
 
+def montyFall(numDoors, rule, trial):
+    #Create a set of doors by using pre-set number of doors
+    doors = [i for i in range (1,numDoors+1)]
+    
+    #Assign the car to a door
+    car = secrets.choice(doors)
+
+    #Guest first choice
+    guest_1st_choice = secrets.choice(doors)
+
+    #Host slips on banana peel so he can accidentally opens the door with the car
+    host_can_open = set(doors) - set([guest_1st_choice])
+   
+    openlist=[]
+    #Host opens all other doors and lefts only one door for the guest as the second choice.
+    for y in range (numDoors-2):
+        host_opens = secrets.choice(list(host_can_open -set(openlist)))
+        #if the door of host accidentlly, the guest will lose
+        if host_opens == car:
+            return False
+            break
+
+        openlist.append(host_opens)
+    
+    guest_2nd_choice = min(set(doors)-set(openlist + [guest_1st_choice]))
+    
+    if trial<=5:
+        print(f"First choice: {guest_1st_choice}, Second choice: {guest_2nd_choice}, Car: {car} ")
+    
+    #Guest Strategies 
+    if rule== 'switch':
+        if car == guest_2nd_choice:
+            result = True
+        else:
+            result = False
+
+    if rule== 'stay':
+        if guest_1st_choice == car:
+            result = True
+        else:
+            result = False
+    
+    return result
+
+
 #Write the result into a csv file and plot a table
 def write_and_plot(list1,list2,list3,list4):
     fieldNames=['3 doors', '6 doors', '9 doors', '20 doors', '100 doors']
-    fileName = "MontyHallResult.csv"
+    fileName = "Result.csv"
     data=[list1[:],list2[:],list3[:],list4[:]]
-    label2=['Win_Switch','Lose_Switch','Win_Stay','Lose_Stay']
+    label2=['Hall_Win_Switch','Fall_Win_Switch','Hall_Win_Stay','Fall_Win_Stay']
 
     with open(fileName, 'w') as csvfile:
         fieldNames.insert(0,'')
@@ -60,22 +107,24 @@ def write_and_plot(list1,list2,list3,list4):
             newElement =label2[data.index(i)]
             i.insert(0,str(newElement))
             writer.writerow(i)
-    plot_table("MontyHallResult.csv")
+    plot_table("Result.csv")
 
 
 
 #Create a bar chart by using the result
-def barChart(list1,list2):
+def barChart(list1,list2,list3, list4):
     fieldNames=['3 doors', '6 doors', '9 doors', '20 doors', '100 doors']
     width = 0.2
     x_list1 = [x-width for x in range(len(list1))]
     x_list2 = [x for x in range(len(list2))]
-    # x_list3 = [x+width for x in range (len(list3))]
-    # x_list4 = [x+width*2 for x in range (len(list4))]
+    x_list3 = [x+width for x in range (len(list3))]
+    x_list4 = [x+width*2 for x in range (len(list4))]
 
     fig,ax = plt.subplots()
-    bar1 = ax.bar(x_list1,list1,width,label='Win_Switch')
-    bar2 = ax.bar(x_list2,list2,width,label='Win_Stay')
+    bar1 = ax.bar(x_list1,list1,width,label='Hall_Win_Switch')
+    bar2 = ax.bar(x_list2,list2,width,label='Fall_Win_Switch')
+    bar3 = ax.bar(x_list3,list3,width,label='Hall_Win_Stay')
+    bar4 = ax.bar(x_list4,list4,width,label='Fall_Win_Stay')
    
 
 
@@ -87,8 +136,8 @@ def barChart(list1,list2):
     #label the bar
     autolabel(bar1,list1)
     autolabel(bar2,list2)
-    # autolabel(bar3,list3)
-    # autolabel(bar4,list4)
+    autolabel(bar3,list3)
+    autolabel(bar4,list4)
 
     x = np.arange(len(fieldNames))
     ax.set_xticks(x)
@@ -96,8 +145,8 @@ def barChart(list1,list2):
     ax.legend()
     ax.set_xlabel('Door Secanrio')
     ax.set_ylabel('Probability of 100000 times')
-    ax.set_title('Monty Hall Swtiching and Staying Scenario Probability')
-    plt.savefig('BarChart.png')
+    ax.set_title(f'Monty Hall & Fall Swtiching and Staying Scenario Probability')
+    plt.savefig(f'BarChart.png')
 
 #create table function
 def plot_table(fileName):
@@ -112,10 +161,10 @@ def plot_table(fileName):
 
 def print_Result(list1,list2,list3,list4):
     write_and_plot(list1,list2,list3,list4)
-    barChart(list1,list3)
+    barChart(list1,list2,list3,list4)
     plt.show()
 
-def plot_probability_distribution(trial_marks, win_probabilities, num_doors, strategy):
+def plot_probability_distribution(module, trial_marks, win_probabilities, num_doors, strategy):
     # Create the figure object
     fig = go.Figure()
 
@@ -154,7 +203,7 @@ def plot_probability_distribution(trial_marks, win_probabilities, num_doors, str
 
     # Update the layout to add titles and axis labels
     fig.update_layout(
-        title=f'Win Probability Convergence Over Trials - {strategy} Strategy',
+        title=f'{module} Win Probability Convergence Over Trials - {strategy} Strategy',
         xaxis_title='Number of Trials',
         yaxis_title='Win Probability',
         legend_title='Number of Doors',
@@ -177,14 +226,19 @@ def main():
     #array for tracing
     trace1 =[[] for _ in numDoors]
     trace2 =[[] for _ in numDoors]
-    #alway switch probaility
-    winProbability1=[]
-    loseProbability1=[]
-    #always Stay probability
-    winProbability2=[]
-    loseProbability2 =[]
+    trace3 =[[] for _ in numDoors]
+    trace4 =[[] for _ in numDoors]
+  
     trial_marks = list(range(1, trials + 1))
 
+    #Monty Hall simulation 
+    #Probabilities Holder Declaration
+    #Swtich
+    winProbability1=[]
+    loseProbability1=[]
+    #Stay 
+    winProbability2=[]
+    loseProbability2 =[]
     for index, val in enumerate(numDoors):
         print(f"Doors = {val}")
         countWin1 = 0
@@ -198,14 +252,9 @@ def main():
             else:
                 countLose1 +=1
         
-        winProbability1.append(round(countWin1/trials,3))
-        loseProbability1.append(round(countLose1/trials,3))
-        # print("Door number: ", n)
-        # print("Win: ", countWin1)
-        # print("Lose: ", countLose1)
-        # print("Always Switching Win probability: ", countWin1/times)
-
-
+        winProbability1.append(round(countWin1/trials,2))
+        loseProbability1.append(round(countLose1/trials,2))
+ 
         #Always Staying Case:
         countWin2 = 0
         countLose2 = 0
@@ -217,15 +266,60 @@ def main():
             else:
                 countLose2 +=1
         
-        winProbability2.append(round(countWin2/trials,3))
-        loseProbability2.append(round(countLose2/trials,3))
+        winProbability2.append(round(countWin2/trials,2))
+        loseProbability2.append(round(countLose2/trials,2))
     
-    print(f"{winProbability1=}")
-    print(f"{winProbability2=}")
+    
+    #Monty Fall simulation
+    #Probabilities Holder Declaration
+    #Switch
+    winProbability3=[]
+    loseProbability3 =[]
+    #Stay
+    winProbability4=[]
+    loseProbability4 =[]
+    for index2, val2 in enumerate(numDoors):
+        print(f"Doors = {val2}")
+        countWin3=0
+        countLose3=0
+        #Always Switching Case:
+        for x in range(trials):
+            if (montyFall(val2, 'switch', x)):
+                countWin3 +=1
+                current_probability = countWin3 / (x + 1)
+                trace3[index2].append(current_probability)
+            else:
+                countLose3 +=1
+        
+        winProbability3.append(round(countWin3/trials,2))
+        loseProbability3.append(round(countLose3/trials,2))
+
+        #Always Staying Case:
+        countWin4 = 0
+        countLose4 = 0
+        for x in range(trials):
+            if (montyFall(val2,'stay',x)):
+                countWin4 +=1
+                current_probability = countWin4 / (x + 1)
+                trace4[index2].append(current_probability)
+            else:
+                countLose4 +=1
+        
+        winProbability4.append(round(countWin4/trials,2))
+        loseProbability4.append(round(countLose4/trials,2))
+   
 
     #print the result
-    print_Result(winProbability1,loseProbability1,winProbability2,loseProbability2)
-    plot_probability_distribution(trial_marks,trace1,numDoors, 'Switch')
-    plot_probability_distribution(trial_marks,trace2,numDoors, 'Stay')
+    print_Result(winProbability1,winProbability3,winProbability2,winProbability4)
+    
+    # print(f"{winProbability1=}")
+    # print(f"{winProbability2=}")
+    # print(f"{winProbability3=}")
+    # print(f"{winProbability4=}")
+
+    # plot_probability_distribution("Monty Hall",trial_marks,trace1,numDoors, 'Switch')
+    # plot_probability_distribution("Monty Hall",trial_marks,trace2,numDoors, 'Stay')
+    plot_probability_distribution("Monty Fall",trial_marks,trace3,numDoors, 'Switch')
+    plot_probability_distribution("Monty Fall",trial_marks,trace4,numDoors, 'Stay')
 if __name__ == "__main__":
     main()
